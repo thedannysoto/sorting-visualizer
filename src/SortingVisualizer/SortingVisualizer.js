64558e10ff7a8897b6519963e7d6f93ae490a214
thedannysoto <87dsoto@gmail.com>
2020-07-08 15:17:21 -0500

    Corrects mergeSort function in SortingVisualizer.js and changes final color style

--- src/SortingVisualizer/SortingVisualizer.js
+++ src/SortingVisualizer/SortingVisualizer.js
@@ -3,6 +3,13 @@ import './SortingVisualizer.css';
 import * as sortingAlgorithms from './sortingAlgorithms';
 import ButtonContainer from './ButtonContainer';
 
+const ANIMATION_SPEED_MS = 3;
+const NUMBER_OF_ARRAY_BARS = 300;
+const PRIMARY_COLOR = 'red';
+const SECONDARY_COLOR = 'white';
+const FINAL_COLOR = 'blue';
+
+
 const SortingVisualizer = props => {
 
     const [barArray, setBarArray] = useState([]);
@@ -11,28 +18,39 @@ const SortingVisualizer = props => {
 
     const resetArray = () => {
         const array = [];
-        for (let i = 0; i < 200; i++) {
+        for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
             array.push(randomIntFromInterval(5, 500));
         }
         setBarArray(array);
+        const arrayBars = document.getElementsByClassName('array-bar');
+        for (let x = 0; x < arrayBars.length; x++) {
+            arrayBars[x].style.backgroundColor = 'red';
+        }
     }
 
     // Algorithms
     const mergeSort = () => {
         const animations = sortingAlgorithms.mergeSort(barArray);
         for (let i = 0; i < animations.length; i++) {
-            const {comparison, swap} = animations[i];
-            setTimeout(() => {
-                const arrayBars = document.getElementsByClassName('array-bar');
-                arrayBars[comparison[1]].style.backgroundColor = 'green';
-                arrayBars[comparison[0]].style.backgroundColor = 'green';
-                setTimeout(() => {
-                    // arrayBars[comparison[1]].style.backgroundColor = 'red';
-                    // arrayBars[comparison[0]].style.backgroundColor = 'red';
-                }, (i + 1) * 10);
-            }, i * 10);
+           const arrayBars = document.getElementsByClassName('array-bar');
+           const isColorChange = i % 3 !== 2;
+           if (isColorChange) {
+               const [barOneIdx, barTwoIdx] = animations[i];
+               const barOneStyle = arrayBars[barOneIdx].style;
+               const barTwoStyle = arrayBars[barTwoIdx].style;
+               const color = i % 3 === 0 ? SECONDARY_COLOR : FINAL_COLOR;
+               setTimeout(() => {
+                   barOneStyle.backgroundColor = color;
+                   barTwoStyle.backgroundColor = color;
+               }, i * ANIMATION_SPEED_MS);
+           } else {
+               setTimeout(() => {
+                   const [barOneIdx, newHeight] = animations[i];
+                   const barOneStyle = arrayBars[barOneIdx].style;
+                   barOneStyle.height = `${newHeight}px`;
+               }, i * ANIMATION_SPEED_MS);
+           }
         }
-
     }
 
     return (
